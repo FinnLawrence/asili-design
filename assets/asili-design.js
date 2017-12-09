ef55ce5476ad2b070954889c0d91c6f688823ce3
Finn Lawrence <finn.lawrence@gmail.com>
2017-12-09 16:30:47 +1300

    Added images modal and changes to cart page

--- assets/asili-design.js
+++ assets/asili-design.js
@@ -2,22 +2,27 @@ $(document).ready(function() {
   $('.button-toggle').click(function() {
     toggleFor($(this));
   });
-  
+
   $('.product-thumbnail').click(function(e){
     e.preventDefault();
     showProductImageFor($(this));
   });
 });
 
+$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+  event.preventDefault();
+  $(this).ekkoLightbox();
+});
+
 function toggleFor(button) {
   button.toggleClass("active");
-  
+
   var target = button.data("target");
   var toggleClass = button.data("toggle");
   var bodyClass = button.data("body-class");
-  
+
   $(target).toggleClass(toggleClass);
-  
+
   if (bodyClass) {
     $('body').toggleClass(bodyClass);
   }
